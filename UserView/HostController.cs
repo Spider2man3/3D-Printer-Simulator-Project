6533e86cab415a8b4985e4c878d23bb7d4889670
Travis Herbert <thcool96@gmail.com>
2018-03-20 17:18:19 -0400

    Setting up Host constroller for sending messages to and from firmware (protocol not implemented)

--- UserView/HostController.cs
+++ UserView/HostController.cs
@@ -10,14 +10,48 @@ namespace UserView
 {
     public class HostController
     {
+        public enum Commands
+        {
+            ResetStepper,
+            StepStepper,
+            MoveGalvonometer,
+            SetLaser
+        }
         PrinterControl controller;
 
         public HostController(PrinterThread pThread)
         {
             this.controller = pThread.GetPrinterSim();
         }
+        private bool execute()
+        {
+            return true;
+        }
 
-        
+        public async void SendCommand(Commands cmd, List<Object> param) //TODO: should be an async task?
+        {
+            var result = false;
+            switch (cmd)
+            {
+                case Commands.ResetStepper:
+                    result = true;// send command
+                    break;
+                case Commands.SetLaser:
+                    result = true;// send command
+                    break;
+                case Commands.StepStepper:
+                    result = true;// send command with param[0] (up or down)
+                    break;
+                case Commands.MoveGalvonometer:
+                    result = true;// send commandwith param[0] x and param[1] y
+                    break;
+                default:
+                    result = false;
+                    break;
+            }
+        }
+
+
 
     }
 }
