043cbf3bf09f3a0e7885bc8ebefe051c2700117c
Adams Bala <adams.alok@gmail.com>
2018-04-11 18:22:12 -0400

    Not sure what these changes are

--- UserView/Helpers/GcodeParser.cs
+++ UserView/Helpers/GcodeParser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hardware;
+using PrinterSimulator;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,68 +11,59 @@ namespace Host
 {
     public class GcodeParser
     {
-        public List<List<string>> ParseGcode(Stream file)
+        public void ParseGcode(Stream file, HostHandler handler)
         {
-            List<List<string>> listOfG = new List<List<string>>();
+            //List<List<string>> listOfG = new List<List<string>>();
 
             var g1 = "G1";
             var g92 = "G92";
-            var lineCounter = 0;
+            var lineCounter = -1;
             using (var sr = new StreamReader(file))
             {
-
                 while (!sr.EndOfStream)
                 {
+                    List<string> GLine = new List<string>();
                     lineCounter++;
                     var line = sr.ReadLine();
                     if (String.IsNullOrEmpty(line)) continue;
                     if (line.IndexOf(g1, StringComparison.CurrentCultureIgnoreCase) >= 0)
-                    {                        
+                    {
                         var eachLine = line.Split(' ');
 
-                        var xString = Array.FindAll(eachLine, x => x.StartsWith("X"));            
+                        var xString = Array.FindAll(eachLine, x => x.StartsWith("X"));
                         var yString = Array.FindAll(eachLine, x => x.StartsWith("Y"));
                         var eString = Array.FindAll(eachLine, x => x.StartsWith("E"));
                         var zString = Array.FindAll(eachLine, x => x.StartsWith("Z"));
-                        List<string> G1Line = new List<string>();
-                        string counterString = lineCounter.ToString();
-                        G1Line.Add(counterString);
-                        if (xString != null && xString.Length != 0)
+
+                        if ((xString != null && xString.Length != 0) && (yString != null && yString.Length != 0))
                         {
-                            G1Line.Add(string.Join("", xString));
-                        }
-                        if (yString != null && yString.Length != 0)
-                        {
-                            G1Line.Add(string.Join("", yString));
+                            var xFloat = float.Parse(xString.First().Substring(1));
+                            var yFloat = float.Parse(yString.First().Substring(1));
+                            handler.execute(Command.MoveGalvonometer, new float[] { xFloat, yFloat });
                         }
                         if (zString != null && zString.Length != 0)
                         {
-                            G1Line.Add(string.Join("", zString));
+                            handler.execute(Command.StepStepper, new float[] { 1f });
                         }
                         if (eString != null && eString.Length != 0)
                         {
-                            G1Line.Add(string.Join("", eString));
+                            var eFloat = float.Parse(eString.First().Substring(1));
+                            if (eFloat == 0f)
+                            {
+                                handler.execute(Command.SetLaser, new float[] { 0f });
+                            }
+                            else
+                            {
+                                handler.execute(Command.SetLaser, new float[] { 1f });
+                            }
                         }
-                        
-                        listOfG.Add(G1Line);
                     }
-                    if (line.IndexOf(g92, StringComparison.CurrentCultureIgnoreCase) >= 0)
+                    else if (line.IndexOf(g92, StringComparison.CurrentCultureIgnoreCase) >= 0)
                     {
-                        var eachLine = line.Split(' ');
-                        var eString = Array.FindAll(eachLine, x => x.StartsWith("E"));
-                        List<string> G92Line = new List<string>();
-                        if (eString != null && eString.Length != 0)
-                        {
-                            G92Line.Add(string.Join("", eString));
-                        }
-                        listOfG.Add(G92Line);
+                        handler.execute(Command.SetLaser, new float[] { 0f });
                     }
-
                 }
-                return listOfG;
             }
-
-
         }
      
     }
